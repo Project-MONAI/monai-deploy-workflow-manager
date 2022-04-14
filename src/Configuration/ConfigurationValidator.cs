a2f791b67b79749699bdb6d21f087ac32eee311a
Jack Schofield <jack.schofield@answerdigital.com>
2022-04-14 11:02:17 +0100

    Add configurationValidator and tests
    Signed-off-by: Jack Schofield <jack.schofield@answerdigital.com>
    Signed-off-by: jackschofield23 <jack.schofield@answerdigital.com>

--- src/Configuration/ConfigurationValidator.cs
+++ src/Configuration/ConfigurationValidator.cs
@@ -31,9 +31,30 @@ namespace Monai.Deploy.WorkflowManager.Configuration
         {
             var valid = true;
 
+            valid &= IsTopicsValid(options.Messaging.Topics);
+
             _validationErrors.ForEach(p => _logger.Log(LogLevel.Error, p));
 
             return valid ? ValidateOptionsResult.Success : ValidateOptionsResult.Fail(string.Join(Environment.NewLine, _validationErrors));
+        }
+
+        public bool IsTopicsValid(MessageBrokerConfigurationKeys configurationKeys)
+        {
+            var valid = true;
+
+            valid &= IsStringValueNotNull(nameof(configurationKeys.WorkflowRequest), configurationKeys.WorkflowRequest);
+            valid &= IsStringValueNotNull(nameof(configurationKeys.ExportRequestPrefix), configurationKeys.ExportRequestPrefix);
+            valid &= IsStringValueNotNull(nameof(configurationKeys.ExportComplete), configurationKeys.ExportComplete);
+
+            return valid;
+        }
+
+        private bool IsStringValueNotNull(string source, string value)
+        {
+            if (value != null) return true;
+
+            _validationErrors.Add($"Value of {source} must not be null.");
+            return false;
         }
     }
 }
