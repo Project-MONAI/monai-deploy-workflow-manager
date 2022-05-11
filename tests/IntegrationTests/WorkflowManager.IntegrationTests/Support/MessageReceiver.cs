197bf3c707a9e7ff150774eec5133f842bf273d2
Joss Sparkes <joss.sparkes@gmail.com>
2022-05-11 10:41:12 +0100

    Added test framework and integration tests (#106)

---
+++ tests/IntegrationTests/WorkflowManager.IntegrationTests/Support/MessageReceiver.cs
@@ -0,0 +1,24 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using RabbitMQ.Client;
+
+namespace Monai.Deploy.WorkflowManager.IntegrationTests.Support
+{
+    [Binding]
+    public class MessageReceiver : DefaultBasicConsumer
+    {
+        private readonly IModel _channel;
+
+        public MessageReceiver(IModel channel)
+        {
+            _channel = channel;
+        }
+
+        public override void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey, IBasicProperties properties, ReadOnlyMemory<byte> body)
+        {
+            // TO DO Assertions
+            _channel.BasicAck(deliveryTag, false);
+        }
+    }
+}
