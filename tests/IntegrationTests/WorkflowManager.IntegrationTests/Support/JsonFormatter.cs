ba5810478484a7234cf95f5e3746d58724ff96d4
Joss Sparkes <joss.sparkes@gmail.com>
2022-05-11 10:41:12 +0100

    Added test framework and integration tests (#106)
    resign commits

---
+++ tests/IntegrationTests/WorkflowManager.IntegrationTests/Support/JsonFormatter.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Monai.Deploy.WorkflowManager.IntegrationTests.Support
+{
+    static class JsonFormatter
+    {
+        public static string FormatJson(string json)
+        {
+            return JToken.Parse(json).ToString(Formatting.Indented);
+        }
+    }
+}
