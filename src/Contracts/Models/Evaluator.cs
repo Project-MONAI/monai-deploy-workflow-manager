7181dab22e94ed222bdfa1134fafda504c715cb1
Lillie Dae <lillie.dae@answerdigital.com>
2022-05-12 12:31:16 +0100

    inital commit
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

--- src/Contracts/Models/Evaluator.cs
+++ src/Contracts/Models/Evaluator.cs
@@ -1,6 +1,8 @@
 ﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
 // SPDX-License-Identifier: Apache License 2.0
 
+using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Monai.Deploy.WorkflowManager.Contracts.Models
@@ -18,5 +20,93 @@ namespace Monai.Deploy.WorkflowManager.Contracts.Models
 
         [JsonProperty(PropertyName = "dicom")]
         public ExecutionContext Dicom { get; set; }
+
+        public static Evaluator Parse(string input)
+        {
+            ReadOnlySpan<char> charSpan = input.Trim().ToCharArray();
+
+            return new Evaluator();
+        }
+    }
+
+
+
+
+
+    public class ConditionalGroup
+    {
+        private readonly List<string> _logicalOperators = new List<string> { "==", "!=", ">", "<", "=>", "=<" };
+
+        Keywords Keyword { get; set; }
+
+        public string LeftParameter { get; set; }
+
+        public string RightParameter { get; set; }
+
+        public ConditionalGroup LeftGroup { get; set; }
+
+        public ConditionalGroup RightGroup { get; set; }
+
+        public void Parse(string input, int currentIndex = 0)
+        {
+            //input = "'F' == {{context.dicom.tags[('0010','0040')]}}";
+            //"AND {{context.dicom.tags[('0010','0040')]}} == 'F'"
+            switch (input)
+            {
+                case "{":
+                    var idxClosingBracket = input.IndexOf('}') + 1;
+                    LeftParameter = input[currentIndex..idxClosingBracket];
+                    break;
+                case "'":
+                    var idxClosingQuote = input.IndexOf('\'') + 1;
+                    RightParameter = input[currentIndex..idxClosingQuote];
+                    break;
+                case "A":
+                case "a":
+                    Parse(input, currentIndex + 1);
+                    break;
+                case "N":
+                case "n":
+                    var npreviousChar = input[currentIndex - 1];
+                    if (npreviousChar == 'a' || npreviousChar == 'A')
+                    {
+                        Parse(input, currentIndex + 1);
+                    }
+                    break;
+                case "D":
+                case "d":
+                    var dpreviousChar = input[currentIndex - 1];
+                    if (dpreviousChar == 'n' || dpreviousChar == 'N')
+                    {
+                        Keyword = Keywords.AND;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static ConditionalGroup Create(string input, int currentIndex = 0)
+        {
+            input = "{{context.dicom.tags[('0010','0040')]}} == 'F'";
+            var conditionalGroup = new ConditionalGroup();
+            conditionalGroup.Parse(input);
+
+            return conditionalGroup;
+
+        }
+
+        public bool Evaluate()
+        {
+            return false;
+        }
+    }
+    /// <summary>
+    /// Group Keywords or Operators
+    /// </summary>
+    enum Keywords
+    {
+        AND,
+        OR,
     }
 }
---
+++ src/WorkflowExecuter.Tests/Common/EvaluatorTests.cs
@@ -0,0 +1,39 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Monai.Deploy.WorkflowManager.Contracts.Models;
+using Xunit;
+
+namespace Monai.Deploy.WorkflowManager.WorkflowExecuter.Tests.Common
+{
+    public class ConditionalGroupTests
+    {
+        //[Theory]
+        //[InlineData(false, "AND {{context.dicom.tags[('0010','0040')]}} == 'F'")]
+        //[InlineData(true, "{{context.dicom.tags[('0010','0040')]}} == 'F'")]
+        ////[InlineData(-4, -6, -10)]
+        ////[InlineData(-2, 2, 0)]
+        ////[InlineData(int.MinValue, -1, int.MaxValue)]
+        //public void ConditionalGroup_CreatesAndEvaluates(bool expected, string input)
+        //{
+        //    var conditional = ConditionalGroup.Create(input);
+        //    var result = conditional.Evaluate();
+        //    Assert.Equal(expected, result);
+        //}
+
+        [Theory]
+        [InlineData("{{context.dicom.tags[('0010','0040')]}}", "F", "{{context.dicom.tags[('0010','0040')]}} == 'F'")]
+        //[InlineData(-4, -6, -10)]
+        //[InlineData(-2, 2, 0)]
+        //[InlineData(int.MinValue, -1, int.MaxValue)]
+        public void ConditionalGroup_CreatesAndEvaluates(string expectedLeftParam, string expectedRightParam, string input)
+        {
+            var conditional = ConditionalGroup.Create(input);
+            var leftParameter = conditional.LeftParameter;
+            var rightParameter = conditional.RightParameter;
+
+            Assert.Equal(expectedLeftParam, leftParameter);
+            Assert.Equal(expectedRightParam, rightParameter);
+        }
+    }
+}
