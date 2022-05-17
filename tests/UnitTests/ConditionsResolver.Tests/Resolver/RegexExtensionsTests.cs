0d6c827a9b0ff615966f6b0f28293a2ed6647992
Lillie Dae <lillie.dae@answerdigital.com>
2022-05-17 13:33:56 +0100

    added regex tests
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ tests/UnitTests/ConditionsResolver.Tests/Resolver/RegexExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using Monai.Deploy.WorkflowManager.ConditionsResolver.Extensions;
+using Xunit;
+
+namespace Monai.Deploy.WorkflowManager.ConditionsResolver.Tests.Resolver
+{
+    public class RegexExtensionsTests
+    {
+        [Theory]
+        [InlineData(new string[] { "test", "(test" }, "test(test")]
+        [InlineData(new string[] { "test", "( test" }, "test( test")]
+        [InlineData(new string[] { "test ", "( test" }, "test ( test")]
+        [InlineData(new string[] { "test ", "( test( test" }, "test ( test( test")]
+        public void Regex_WhenSplitOnce_ShouldOnlyHaveArrayOfTwo(string[] expected, string stringToSplit)
+        {
+            var regexFindBrackets = new Regex(@"((?<!\[)\()");
+            var result = regexFindBrackets.SplitOnce(stringToSplit);
+            Assert.Equal(expected, result);
+            Assert.Equal(2, result.Length);
+        }
+    }
+}
