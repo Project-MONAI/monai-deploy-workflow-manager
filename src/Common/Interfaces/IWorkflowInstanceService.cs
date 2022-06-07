9caa3fe3376c32af9ba97599a2e3246064055f8b
Jack Schofield <jack.schofield@answerdigital.com>
2022-06-07 15:40:03 +0100

    add get workflow instances
    Signed-off-by: Jack Schofield <jack.schofield@answerdigital.com>

---
+++ src/Common/Interfaces/IWorkflowInstanceService.cs
@@ -0,0 +1,12 @@
+﻿using Monai.Deploy.WorkloadManager.Contracts.Models;
+
+namespace Monai.Deploy.WorkflowManager.Common.Interfaces
+{
+    public interface IWorkflowInstanceService
+    {
+        /// <summary>
+        /// Gets a list of workflow instances from the workflow instance repository.
+        /// </summary>
+        Task<IList<WorkflowInstance>> GetListAsync();
+    }
+}
---
+++ src/WorkflowManager/Controllers/WorkflowInstanceController.cs
@@ -0,0 +1,55 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Monai.Deploy.WorkflowManager.Common.Interfaces;
+
+namespace Monai.Deploy.WorkloadManager.Controllers;
+
+/// <summary>
+/// Workflow Instances Controller.
+/// </summary>
+[ApiController]
+[Route("workflowinstances")]
+public class WorkflowInstanceController : ControllerBase
+{
+    private readonly IWorkflowInstanceService _workflowInstanceService;
+
+    private readonly ILogger<WorkflowInstanceController> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WorkflowInstanceController"/> class.
+    /// </summary>
+    /// <param name="workflowInstanceService"></param>
+    public WorkflowInstanceController(
+        IWorkflowInstanceService workflowInstanceService,
+        ILogger<WorkflowInstanceController> logger)
+    {
+        _workflowInstanceService = workflowInstanceService ?? throw new ArgumentNullException(nameof(workflowInstanceService));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    /// <summary>
+    /// Get a list of workflowInstances.
+    /// </summary>
+    /// <returns>A list of workflow instances.</returns>
+    [HttpGet]
+    public async Task<IActionResult> GetListAsync()
+    {
+        try
+        {
+            var workflowsInstances = await _workflowInstanceService.GetListAsync();
+
+            return Ok(workflowsInstances);
+        }
+        catch (Exception e)
+        {
+            this._logger.LogError($"{nameof(GetListAsync)} - Failed to get workflowInstances", e);
+
+            return Problem($"Unexpected error occured: {e.Message}", $"/workflowinstances", 500);
+        }
+    }
+}
