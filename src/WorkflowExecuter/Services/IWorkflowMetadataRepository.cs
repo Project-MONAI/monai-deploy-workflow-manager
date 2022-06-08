d96c7b6b3f26c69b4577c913e4c01105c2dc00a5
Lillie Dae <lillie.dae@answerdigital.com>
2022-06-08 09:24:02 +0100

    changes to implement metadata store for workflow manager  #124
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/WorkflowExecuter/Services/IWorkflowMetadataRepository.cs
@@ -0,0 +1,10 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+namespace Monai.Deploy.WorkloadManager.WorkfowExecuter.Services
+{
+    public interface IWorkflowMetadataRepository
+    {
+        Task<bool> UpdateForTaskAsync(string workflowInstanceId, List<TaskMetadata> metadata);
+    }
+}
---
+++ src/WorkflowExecuter/Services/WorkflowMetaData.cs
@@ -0,0 +1,45 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Ardalis.GuardClauses;
+using Newtonsoft.Json;
+
+namespace Monai.Deploy.WorkloadManager.WorkfowExecuter.Services
+{
+    public class WorkflowMetaData
+    {
+        [JsonProperty(PropertyName = "workflowInstanceId")]
+        public string? WorkflowInstanceId { get; set; }
+
+
+        [JsonProperty(PropertyName = "tasks")]
+        public List<TaskMetadata>? Metadata { get; set; }
+    }
+
+    public class TaskMetadata
+    {
+        [JsonProperty(PropertyName = "taskId")]
+        public string? TaskId { get; set; }
+
+        [JsonProperty(PropertyName = "taskExecutionId")]
+        public string? TaskExecutionId { get; set; }
+
+        [JsonProperty(PropertyName = "metadata")]
+        public Dictionary<string, object>? Metadata { get; set; }
+
+        public static List<TaskMetadata> Create(Dictionary<string, object> metadata, string executionId, string taskId)
+        {
+            Guard.Against.NullOrEmpty(metadata, nameof(metadata));
+            Guard.Against.NullOrEmpty(taskId, nameof(taskId));
+            Guard.Against.NullOrEmpty(executionId, nameof(executionId));
+
+            var taskMetadata = new TaskMetadata
+            {
+                Metadata = metadata,
+                TaskExecutionId = executionId,
+                TaskId = taskId
+            };
+            return new List<TaskMetadata> { taskMetadata };
+        }
+    }
+}
