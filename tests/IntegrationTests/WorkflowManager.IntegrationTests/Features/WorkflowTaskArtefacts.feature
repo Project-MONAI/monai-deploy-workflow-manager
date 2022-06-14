c9f439048c4101a7a4014edd266a7bb369b709be
Joe Batt <joe.batt@answerdigital.com>
2022-06-14 10:44:07 +0100

    Downgrade of MinIO client
    Signed-off-by: Joe Batt <joe.batt@answerdigital.com>

---
+++ tests/IntegrationTests/WorkflowManager.IntegrationTests/Features/WorkflowTaskArtefacts.feature
@@ -0,0 +1,47 @@
+﻿Feature: WorkflowTaskArtefact
+
+Artefacts can get passed into and between tasks
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket exists in MinIO, publish workflow request which uses input artefacts
+    Given I have a bucket in MinIO dicom
+    And I have a clinical workflow Single_Task_Context_Input
+    When I publish a Workflow Request Message Context_Input_AE
+    Then I can see 1 Workflow Instance is created 
+    And 1 Task Dispatch event is published
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket does not exist in MinIO, publish workflow request which uses non existant bucket
+    Given I have a clinical workflow Single_Task_Context_Input
+    When I publish a Workflow Request Message Context_Input_AE
+    Then The workflow instance fails
+
+@WorkflowTaskArtefacts
+Scenario Outline: Create artefact in MinIO, publish task update message with artefact as output
+    Given I have a bucket in MinIO outputArtefact
+    And I have a clinical workflow Multi_Task_Output_Artifact
+    And I have a Workflow Instance Single_Task_Completed
+    When I publish a task update message output_artefact
+    When I publish a task update message <Task_Update_Message>
+    Then I can see 1 Workflow Instance is created 
+    And 1 Task Dispatch event is published
+    Examples:
+    | Task_Update_Message  |
+    | output_artefact_file |
+    | output_artefact_dir  |
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket exists in MinIO, send task dispatch with non existant file path
+    Given I have a bucket in MinIO dicom
+    And I have a clinical workflow Single_Task_Context_Input
+    When I publish a Workflow Request Message Context_Input_AE
+    When I publish a task update message output_artefact
+    Then The workflow instance fails
+
+@WorkflowTaskArtefacts
+Scenario Outline: Bucket exists in MinIO, send task dispatch with non existant artefact
+    Given I have a bucket in MinIO dicom
+    And I have a clinical workflow Single_Task_Context_Input
+    And I have a Workflow Instance non_existant_artefact
+    When I publish a Workflow Request Message Context_Input_AE
+    Then The workflow instance fails
---
+++ tests/IntegrationTests/WorkflowManager.IntegrationTests/Support/MinioClientUtil.cs
@@ -0,0 +1,106 @@
+﻿using Minio;
+using Minio.DataModel;
+using Monai.Deploy.WorkflowManager.IntegrationTests.POCO;
+using Polly;
+using Polly.Retry;
+
+namespace Monai.Deploy.WorkflowManager.IntegrationTests.Support
+{
+    public class MinioClientUtil
+    {
+        private AsyncRetryPolicy RetryPolicy { get; set; }
+        private MinioClient Client { get; set; }
+
+        public MinioClientUtil()
+        {
+            Client = new MinioClient()
+                .WithEndpoint(TestExecutionConfig.MinioConfig.Endpoint)
+                .WithCredentials(TestExecutionConfig.MinioConfig.AccessKey, TestExecutionConfig.MinioConfig.AccessToken)
+                .Build();
+            RetryPolicy = Policy.Handle<Exception>().WaitAndRetryAsync(retryCount: 10, sleepDurationProvider: _ => TimeSpan.FromMilliseconds(500));
+        }
+
+        public async Task CreateBucket(string bucketName)
+        {
+            await RetryPolicy.ExecuteAsync(async () =>
+            {
+                try
+                {
+                    await Client.MakeBucketAsync(
+                        new MakeBucketArgs()
+                            .WithBucket(bucketName)
+                    );
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[Bucket]  Exception: {e}");
+                }
+            });
+        }
+
+        public async Task AddFileToStorage(string fileLocation, string bucketName, string objectName)
+        {
+            await RetryPolicy.ExecuteAsync(async () =>
+            {
+                try
+                {
+                    byte[] bs = File.ReadAllBytes(fileLocation);
+                    using (MemoryStream filestream = new MemoryStream(bs))
+                    {
+                        FileInfo fileInfo = new FileInfo(fileLocation);
+                        var metaData = new Dictionary<string, string>
+                    {
+                                { "Test-Metadata", "Test  Test" }
+                    };
+                        PutObjectArgs args = new PutObjectArgs()
+                                                        .WithBucket(bucketName)
+                                                        .WithObject(objectName)
+                                                        .WithStreamData(filestream)
+                                                        .WithObjectSize(filestream.Length)
+                                                        .WithContentType("application/octet-stream")
+                                                        .WithHeaders(metaData);
+                        await Client.PutObjectAsync(args);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[Bucket]  Exception: {e}");
+                }
+            });
+        }
+
+        public async Task<ObjectStat> GetFile(string bucketName, string objectName, string fileName)
+        {
+            var args = new GetObjectArgs()
+                                         .WithBucket(bucketName)
+                                         .WithObject(objectName)
+                                         .WithFile(fileName);
+            return await Client.GetObjectAsync(args);
+        }
+
+        public async Task DeleteBucket(string bucketName)
+        {
+            bool found = await Client.BucketExistsAsync(bucketName);
+            if (found)
+            {
+                await RetryPolicy.ExecuteAsync(async () =>
+                {
+                    var args = new RemoveBucketArgs().WithBucket(bucketName);
+                    await Client.RemoveBucketAsync(args);
+                });
+            }
+        }
+
+        public async Task RemoveObjects(string bucketName, string objectName)
+        {
+            bool found = await Client.BucketExistsAsync(bucketName);
+            if (found)
+            {
+                var args = new RemoveObjectArgs()
+                                         .WithBucket(bucketName)
+                                         .WithObject(objectName);
+                await Client.RemoveObjectAsync(args);
+            }
+        }
+    }
+}
