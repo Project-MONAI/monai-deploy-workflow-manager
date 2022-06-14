0486836f21a0d155292bbab92aba9d23d8ccf821
Lillie Dae <lillie.dae@answerdigital.com>
2022-06-14 09:39:11 +0100

    changes required to make integration tests work
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/WorkflowExecuter/Common/IConditionalParameterParser.cs
@@ -0,0 +1,15 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Monai.Deploy.WorkloadManager.Contracts.Models;
+
+namespace Monai.Deploy.WorkloadManager.WorkfowExecuter.Common
+{
+    public interface IConditionalParameterParser
+    {
+        WorkflowInstance? WorkflowInstance { get; }
+
+        string ResolveParameters(string conditions, WorkflowInstance workflowInstance);
+        bool TryParse(string conditions, WorkflowInstance workflowInstance);
+    }
+}
