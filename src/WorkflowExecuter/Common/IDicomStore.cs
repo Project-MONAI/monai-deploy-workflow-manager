2920f0679df386bfb63df8913265bee26a8059f6
Lillie Dae <lillie.dae@answerdigital.com>
2022-06-21 13:58:01 +0100

    dicom store interface
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/WorkflowExecuter/Common/IDicomStore.cs
@@ -0,0 +1,18 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Monai.Deploy.Storage;
+using Monai.Deploy.WorkflowManager.Contracts.Models;
+
+namespace Monai.Deploy.WorkloadManager.WorkfowExecuter.Common
+{
+    public interface IDicomStore
+    {
+        IStorageService StorageService { get; }
+
+        string GetAllValue(string keyId, string payloadId, string bucketId);
+        string GetAnyValue(string keyId, string payloadId, string bucketId);
+        string GetFirstValue(string payloadId, string bucketId, string keyId);
+        string GetDcmFileValueAtIndex(int index, string path, string bucketId, string keyId);
+    }
+}
