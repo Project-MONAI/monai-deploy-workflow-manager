6fd165b7e332f8344dddbdb1d802f9884780caac
Lillie Dae <lillie.dae@answerdigital.com>
2022-06-28 10:36:13 +0100

    IDicomStore
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/WorkflowExecuter/Common/IDicomStore.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Monai.Deploy.Storage.API;
+
+namespace Monai.Deploy.WorkloadManager.WorkfowExecuter.Common
+{
+    public interface IDicomStore
+    {
+        IStorageService StorageService { get; }
+
+        Task<string> GetAllValueAsync(string keyId, string payloadId, string bucketId);
+        Task<string> GetAnyValueAsync(string keyId, string payloadId, string bucketId);
+        Task<string> GetDcmJsonFileValueAtIndexAsync(int index, string path, string bucketId, string keyId, List<VirtualFileInfo> items);
+        Task<string> GetFirstValueAsync(IList<VirtualFileInfo> items, string payloadId, string bucketId, string keyId);
+    }
+}
