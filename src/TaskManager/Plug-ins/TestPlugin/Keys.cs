191d865ef4001b16088b2f1e6671c39dcf0937cc
Jack Schofield <jack.schofield@answerdigital.com>
2022-06-30 11:15:31 +0100

    add test plugin
    Signed-off-by: Jack Schofield <jack.schofield@answerdigital.com>

---
+++ src/TaskManager/Plug-ins/TestPlugin/Keys.cs
@@ -0,0 +1,23 @@
+﻿namespace Monai.Deploy.WorkflowManager.TaskManager.TestPlugin
+{
+    internal static class Keys
+    {
+        /// <summary>
+        /// Key for the status to return.
+        /// </summary>
+        public static readonly string ExecuteTaskStatus = "executetaskstatus";
+
+        /// <summary>
+        /// Key for the status to return.
+        /// </summary>
+        public static readonly string GetStatusStatus = "getstatusstatus";
+
+        /// <summary>
+        /// Required arguments to run the Argo workflow.
+        /// </summary>
+        public static readonly IReadOnlyList<string> RequiredParameters =
+            new List<string> {
+                ExecuteTaskStatus
+            };
+    }
+}
