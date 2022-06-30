f4eb55313a31b97f6ee5289bfad0b98bb09b70e6
Lillie Dae <lillie.dae@answerdigital.com>
2022-06-30 17:31:39 +0100

    new files
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/Common/Extensions/TaskDispatchEventExtension.cs
@@ -0,0 +1,32 @@
+﻿// SPDX-FileCopyrightText: © 2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Monai.Deploy.Messaging.Events;
+using Newtonsoft.Json;
+
+namespace Monai.Deploy.WorkflowManager.Common.Extensions
+{
+    public static class TaskDispatchEventExtension
+    {
+        public static string? GetTaskPluginArgumentsParameter(this TaskDispatchEvent taskDispatchEvent, string key)
+        {
+            taskDispatchEvent.TaskPluginArguments.TryGetValue(key, out string? value);
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            return value;
+        }
+
+        public static T? GetTaskPluginArgumentsParameter<T>(this TaskDispatchEvent taskDispatchEvent, string key)
+        {
+            taskDispatchEvent.TaskPluginArguments.TryGetValue(key, out string? value);
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return default(T);
+            }
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
---
+++ src/TaskManager/Plug-ins/Argo/StaticValues/Keys.cs
@@ -0,0 +1,82 @@
+﻿// SPDX-FileCopyrightText: © 2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+namespace Monai.Deploy.WorkflowManager.TaskManager.Argo.StaticValues
+{
+    internal static class Keys
+    {
+        /// <summary>
+        /// Key for the namespace where the Argo workflows are stored and executed.
+        /// </summary>
+        public static readonly string Namespace = "namespace";
+
+        /// <summary>
+        /// Key for the endpoint where the Argo server is running.
+        /// </summary>
+        public static readonly string BaseUrl = "baseUrl";
+
+        /// <summary>
+        /// Key for the name of the main 'WorkflowTemplate' stored on the targeted Argo server.
+        /// </summary>
+        public static readonly string WorkflowTemplateName = "workflowTemplateName";
+
+        /// <summary>
+        /// Key for the setting 'ActiveDeadlineSeconds' of a Argo workflow.
+        /// </summary>
+        public static readonly string TimeoutSeconds = "timeoutSeconds";
+
+        /// <summary>
+        /// Key for setting the API token to authenticate to the Argo server.
+        /// </summary>
+        public static readonly string ArgoApiToken = "apiToken";
+
+        /// <summary>
+        /// Key for setting the message broker's endpoint.
+        /// </summary>
+        public static readonly string MessagingEnddpoint = "messagingEndpoint";
+
+        /// <summary>
+        /// Key for setting the user name to access the message broker.
+        /// </summary>
+        public static readonly string MessagingUsername = "messagingUsername";
+
+        /// <summary>
+        /// Key for setting the password to access the message broker.
+        /// </summary>
+        public static readonly string MessagingPassword = "messagingPassword";
+
+        /// <summary>
+        /// Key for setting the topic of the completion event.
+        /// </summary>
+        public static readonly string MessagingTopic = "messagingTopic";
+
+        /// <summary>
+        /// Key for setting the exchange of the message broker.
+        /// </summary>
+        public static readonly string MessagingExchange = "messagingExchange";
+
+        /// <summary>
+        /// Key for resource limitations
+        /// </summary>
+        public static readonly string ArgoResource = "resources";
+
+        /// <summary>
+        /// Key for priority classnames on task plugin arguments side
+        /// </summary>
+        public static readonly string TaskPriorityClassName = "priority_class";
+
+        /// <summary>
+        /// Required arguments to run the Argo workflow.
+        /// </summary>
+        public static readonly IReadOnlyList<string> RequiredParameters =
+            new List<string> {
+                BaseUrl,
+                WorkflowTemplateName,
+                MessagingEnddpoint,
+                MessagingUsername,
+                MessagingPassword,
+                MessagingTopic,
+                MessagingExchange,
+            };
+    }
+}
