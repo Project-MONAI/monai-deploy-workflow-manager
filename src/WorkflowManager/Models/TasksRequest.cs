73b5f607ceeb1bbd9f9c2fc98d2483131b1f0077
Lillie Dae <lillie.dae@answerdigital.com>
2022-07-28 13:16:35 +0100

    minor fix
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/WorkflowManager/Models/TasksRequest.cs
@@ -0,0 +1,31 @@
+﻿// SPDX-FileCopyrightText: © 2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Newtonsoft.Json;
+
+namespace Monai.Deploy.WorkflowManager.Models
+{
+    /// <summary>
+    /// TasksRequest model.
+    /// </summary>
+    public class TasksRequest
+    {
+        /// <summary>
+        /// Gets or sets WorkflowInstanceId.
+        /// </summary>
+        [JsonProperty(PropertyName = "workflowInstanceId")]
+        public string WorkflowInstanceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets TaskId.
+        /// </summary>
+        [JsonProperty(PropertyName = "taskId")]
+        public string TaskId { get; set; }
+
+        /// <summary>
+        /// Gets or sets ExecutionId.
+        /// </summary>
+        [JsonProperty(PropertyName = "executionId")]
+        public string ExecutionId { get; set; }
+    }
+}
---
+++ tests/UnitTests/WorkflowManager.Common.Tests/Services/TasksServiceTests.cs
@@ -0,0 +1,60 @@
+﻿// SPDX-FileCopyrightText: © 2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using FluentAssertions;
+using Monai.Deploy.Messaging.Events;
+using Monai.Deploy.WorkflowManager.Common.Interfaces;
+using Monai.Deploy.WorkflowManager.Common.Services;
+using Monai.Deploy.WorkflowManager.Contracts.Models;
+using Monai.Deploy.WorkflowManager.Database.Interfaces;
+using Moq;
+using Xunit;
+
+namespace Monai.Deploy.WorkflowManager.Test.Services
+{
+    public class TasksServiceTests
+    {
+        private readonly Mock<ITasksRepository> _tasksRepository;
+
+        private ITasksService TasksService { get; set; }
+
+        public TasksServiceTests()
+        {
+            _tasksRepository = new Mock<ITasksRepository>();
+
+            TasksService = new TasksService(_tasksRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetListAsync_TasksExist_ReturnsList()
+        {
+            var expectedTaskId = Guid.NewGuid().ToString();
+            var expectedExecutionId = Guid.NewGuid().ToString();
+            var expectedWorkflowId = Guid.NewGuid().ToString();
+
+            var taskExecution = new List<TaskExecution> {
+                new TaskExecution
+                {
+                    ExecutionId = expectedExecutionId,
+                    TaskId = expectedTaskId,
+                    Status = TaskExecutionStatus.Dispatched
+                }
+            };
+
+            _tasksRepository.Setup(tr => tr.GetAllAsync(It.IsAny<int?>(), It.IsAny<int?>()))
+                .ReturnsAsync(() => taskExecution);
+
+            var result = await TasksService.GetAllAsync();
+
+            result.Should().NotBeNull();
+            result.Count.Should().Be(1);
+
+            var objectResult = Assert.IsType<TaskExecution>(result[0]);
+
+            objectResult.Should().NotBeNull();
+            objectResult.ExecutionId.Should().Be(expectedExecutionId);
+            objectResult.TaskId.Should().Be(expectedTaskId);
+            objectResult.Status.Should().Be(TaskExecutionStatus.Dispatched);
+        }
+    }
+}
