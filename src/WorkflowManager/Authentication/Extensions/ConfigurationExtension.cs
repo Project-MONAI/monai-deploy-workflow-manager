92a7913c7c952ef1e8ba80381bca4e417ea0661f
Lillie Dae <lillie.dae@answerdigital.com>
2022-08-17 18:48:21 +0100

    api authentication implementation
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/WorkflowManager/Authentication/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Copyright 2022 MONAI Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Microsoft.Extensions.Configuration;
+using Ardalis.GuardClauses;
+
+namespace Monai.Deploy.WorkflowManager.Authentication.Extensions
+{
+    public static class ConfigurationExtension
+    {
+        /// <summary>
+        /// Gets configuration values from the IConfiguration provider.
+        /// under "WorkflowManagerAuthentication" key you are able to
+        /// have any label of configuration for example could have 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="serverRealm"></param>
+        /// <param name="serverRealmKey"></param>
+        /// <param name="requiredUserRoles"></param>
+        /// <param name="requiredAdminRoles"></param>
+        public static void GetAndValidateConfig(
+            this IConfiguration configuration,
+            out string serverRealm,
+            out string serverRealmKey,
+            out Dictionary<string, string>[] requiredUserRoles,
+            out Dictionary<string, string>[] requiredAdminRoles)
+        {
+            Guard.Against.Null(configuration, nameof(configuration));
+
+            var authenticationSettings = configuration.GetSection("WorkflowManagerAuthentication");
+            Guard.Against.Null(authenticationSettings, nameof(authenticationSettings), "Missing WorkflowManagerAuthentication section in config.");
+
+            var authenticationSettingsSection = authenticationSettings.GetSection("OpenId");
+            Guard.Against.Null(authenticationSettings, nameof(authenticationSettings), "OpenId section not found.");
+
+            serverRealm = authenticationSettingsSection["ServerRealm"];
+            Guard.Against.NullOrWhiteSpace(serverRealm, nameof(serverRealm), "SeverRealm is a required authenticationSettings attribute.");
+            serverRealmKey = authenticationSettingsSection["ServerRealmKey"];
+            Guard.Against.NullOrWhiteSpace(serverRealm, nameof(serverRealmKey), "SeverRealmKey is a required authenticationSettings attribute.");
+
+            var requiredClaims = authenticationSettingsSection.GetSection("Claims");
+            requiredUserRoles = requiredClaims.GetSection("RequiredUserClaims").Get<Dictionary<string, string>[]>();
+            requiredAdminRoles = requiredClaims.GetSection("RequiredAdminClaims").Get<Dictionary<string, string>[]>();
+        }
+    }
+}
---
+++ src/WorkflowManager/WorkflowManager/Controllers/PayloadsController.cs
@@ -0,0 +1,132 @@
+﻿/*
+ * Copyright 2022 MONAI Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Monai.Deploy.WorkflowManager.Common.Interfaces;
+using Monai.Deploy.WorkflowManager.Configuration;
+using Monai.Deploy.WorkflowManager.Filter;
+using Monai.Deploy.WorkflowManager.Services;
+
+namespace Monai.Deploy.WorkflowManager.Controllers
+{
+    /// <summary>
+    /// Payloads Controller.
+    /// </summary>
+    [ApiController]
+    [Route("payloads")]
+    public class PayloadsController : ApiControllerBase
+    {
+        private readonly IOptions<WorkflowManagerOptions> _options;
+        private readonly IPayloadService _payloadService;
+
+        private readonly ILogger<PayloadsController> _logger;
+        private readonly IUriService _uriService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PayloadsController"/> class.
+        /// </summary>
+        /// <param name="payloadService">paylod service to retrieve payloads.</param>
+        /// <param name="logger">logger.</param>
+        /// <param name="uriService">Uri Service.</param>
+        /// <param name="options">Workflow Manager options.</param>
+        public PayloadsController(
+            IPayloadService payloadService,
+            ILogger<PayloadsController> logger,
+            IUriService uriService,
+            IOptions<WorkflowManagerOptions> options)
+        : base(options)
+        {
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+            _payloadService = payloadService ?? throw new ArgumentNullException(nameof(payloadService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _uriService = uriService ?? throw new ArgumentNullException(nameof(uriService));
+        }
+
+        /// <summary>
+        /// Gets a paged response list of all workflows.
+        /// </summary>
+        /// <param name="filter">Filters.</param>
+        /// <param name="patientId">Optional paient Id.</param>
+        /// <param name="patientName">Optional patient name.</param>
+        /// <returns>paged response of subset of all workflows.</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync([FromQuery] PaginationFilter filter, [FromQuery] string patientId = "", [FromQuery] string patientName = "")
+        {
+            try
+            {
+                var route = Request?.Path.Value ?? string.Empty;
+                var pageSize = filter.PageSize ?? _options.Value.EndpointSettings.DefaultPageSize;
+                var validFilter = new PaginationFilter(filter.PageNumber, pageSize, _options.Value.EndpointSettings.MaxPageSize);
+
+                var pagedData = await _payloadService.GetAllAsync(
+                    (validFilter.PageNumber - 1) * validFilter.PageSize,
+                    validFilter.PageSize,
+                    patientId,
+                    patientName);
+
+                var dataTotal = await _payloadService.CountAsync();
+                var pagedReponse = CreatePagedReponse(pagedData.ToList(), validFilter, dataTotal, _uriService, route);
+
+                return Ok(pagedReponse);
+            }
+            catch (Exception e)
+            {
+                return Problem($"Unexpected error occured: {e.Message}", $"/payload", InternalServerError);
+            }
+        }
+
+        /// <summary>
+        /// Get a workflow by the ID.
+        /// </summary>
+        /// <param name="id">The Workflow Id.</param>
+        /// <returns>The specified workflow for a given Id.</returns>
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromRoute] string id)
+        {
+            if (string.IsNullOrWhiteSpace(id) || !Guid.TryParse(id, out _))
+            {
+                _logger.LogDebug($"{nameof(GetAsync)} - Failed to validate {nameof(id)}");
+
+                return Problem($"Failed to validate {nameof(id)}, not a valid guid", $"/payload/{id}", BadRequest);
+            }
+
+            try
+            {
+                var payload = await _payloadService.GetByIdAsync(id);
+
+                if (payload is null)
+                {
+                    _logger.LogDebug($"{nameof(GetAsync)} - Failed to find payload with payload id: {id}");
+
+                    return NotFound($"Faild to find payload with payload id: {id}");
+                }
+
+                return Ok(payload);
+            }
+            catch (Exception e)
+            {
+                return Problem($"Unexpected error occured: {e.Message}", $"/payload/{nameof(id)}", InternalServerError);
+            }
+        }
+    }
+}
