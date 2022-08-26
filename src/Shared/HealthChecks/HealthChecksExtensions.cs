5cc3b0ae5f2ed428708153512c1a536b557402a9
Lillie Dae <lillie.dae@answerdigital.com>
2022-08-26 13:59:35 +0100

    minor fixes
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/Shared/HealthChecks/HealthChecksExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Monai.Deploy.Messaging.Configuration;
+using Monai.Deploy.WorkflowManager.Configuration;
+using RabbitMQ.Client;
+
+namespace Monai.Deploy.WorkflowManager.HealthChecks
+{
+    public static class HealthChecksExtensions
+    {
+        public static void AddMonaiHealthChecks(this IServiceCollection services, IOptions<WorkloadManagerDatabaseSettings> dbSettings, IConnectionFactory subscriberQueueFactory, IConnectionFactory publisherQueueFactory, HealthStatus failiureStatus) =>
+            services.AddHealthChecks()
+                        .AddMongoDb(
+                            dbSettings.Value.ConnectionString,
+                            HealthCheckSettings.DatabaseHealthCheckName,
+                            failiureStatus,
+                            HealthCheckSettings.DatabaseTags,
+                            HealthCheckSettings.DatabaseHealthCheckTimeout)
+                        .AddRabbitMQ(
+                            _ => subscriberQueueFactory,
+                            HealthCheckSettings.SubscriberQueueHealthCheckName,
+                            failiureStatus,
+                            HealthCheckSettings.SubscriberQueueTags,
+                            HealthCheckSettings.SubscriberQueueHealthCheckTimeout)
+                        .AddRabbitMQ(
+                            _ => publisherQueueFactory,
+                            HealthCheckSettings.PublisherQueueHealthCheckName,
+                            failiureStatus,
+                            HealthCheckSettings.PublisherQueueTags,
+                            HealthCheckSettings.PublisherQueueHealthCheckTimeout);
+
+
+        public static void GetRequiredServicesForHealthChecks(
+            this IServiceCollection services,
+            out IOptions<WorkloadManagerDatabaseSettings> dbSettings,
+            out IConnectionFactory subscriberQueueFactory,
+            out IConnectionFactory publisherQueueFactory)
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8601 // Possible null reference assignment.
+            // Should blow up in configuration is wrong.
+            var sp = services.BuildServiceProvider();
+            dbSettings = sp.GetService<IOptions<WorkloadManagerDatabaseSettings>>();
+            var messageBrokerConfig = sp.GetService<IOptions<MessageBrokerServiceConfiguration>>();
+
+            subscriberQueueFactory = RabbitHealthCheckFactory.Create(messageBrokerConfig.Value.SubscriberSettings);
+            publisherQueueFactory = RabbitHealthCheckFactory.Create(messageBrokerConfig.Value.PublisherSettings);
+#pragma warning restore CS8601 // Possible null reference assignment.
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+    }
+}
---
+++ tests/UnitTests/HealthChecks.Tests/HealthCheckTests.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Copyright 2021-2022 MONAI Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Monai.Deploy.WorkflowManager.HealthChecks.Tests
+{
+    public class HealthCheckTests
+    {
+        [Fact]
+        public async Task HealthCheckResponse_GivenData_ShouldHaveExpectedData()
+        {
+            var status = "Healthy";
+            var component = "component";
+            var description = "description";
+            var tags = new[] { "tag1", "tag2" };
+
+            var healthCheck = new HealthCheck
+            {
+                Status = status,
+                Component = component,
+                Description = description,
+                Tags = tags,
+            };
+
+            var responseStatus = "Healthy";
+            var checks = new[] { healthCheck };
+            var time = TimeSpan.FromSeconds(5);
+
+            var response = new HealthCheckResponse()
+            {
+                Status = responseStatus,
+                Checks = checks,
+                Duration = time,
+            };
+
+            Assert.Equal(responseStatus, response.Status);
+            Assert.Equal(time, response.Duration);
+            Assert.Contains(healthCheck, response.Checks);
+        }
+
+        [Fact]
+        public async Task HealthCheck_GivenData_ShouldHaveExpectedData()
+        {
+            var status = "Healthy";
+            var component = "component";
+            var description = "description";
+            var tags = new[] { "tag1", "tag2" };
+
+            var healthCheck = new HealthCheck
+            {
+                Status = status,
+                Component = component,
+                Description = description,
+                Tags = tags,
+            };
+
+            Assert.Equal(healthCheck.Status, status);
+            Assert.Equal(healthCheck.Component, component);
+            Assert.Equal(healthCheck.Description, description);
+            Assert.Equal(healthCheck.Tags, tags);
+        }
+
+    }
+}
