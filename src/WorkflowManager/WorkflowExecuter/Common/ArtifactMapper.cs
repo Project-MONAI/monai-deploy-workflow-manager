2cb35a23bd7452bb8cb48aa419b7adf9e8d545f8
Jack Schofield <jack.schofield@answerdigital.com>
2022-09-22 15:02:19 +0100

    add output logging
    Signed-off-by: Jack Schofield <jack.schofield@answerdigital.com>

--- src/WorkflowManager/WorkflowExecuter/Common/ArtifactMapper.cs
+++ src/WorkflowManager/WorkflowExecuter/Common/ArtifactMapper.cs
@@ -70,12 +70,12 @@ namespace Monai.Deploy.WorkflowManager.WorkfowExecuter.Common
                 {
                     artifactPathDictionary.Add(mappedArtifact.Key, mappedArtifact.Value);
 
-                    _logger.LogArtifactPassing(artifact, mappedArtifact.Value, shouldExistYet ? "Input" : "Output", true);
+                    _logger.LogArtifactPassing(artifact, mappedArtifact.Value, shouldExistYet ? "Input" : "Pre-Task Output Path Mapping", true);
 
                     continue;
                 }
 
-                _logger.LogArtifactPassing(artifact, mappedArtifact.Value, shouldExistYet ? "Input" : "Output", false);
+                _logger.LogArtifactPassing(artifact, mappedArtifact.Value, shouldExistYet ? "Input" : "Pre-Task Output Path Mapping", false);
 
                 if (artifact.Mandatory)
                 {
--- src/WorkflowManager/WorkflowExecuter/Services/WorkflowExecuterService.cs
+++ src/WorkflowManager/WorkflowExecuter/Services/WorkflowExecuterService.cs
@@ -423,13 +423,17 @@ namespace Monai.Deploy.WorkflowManager.WorkfowExecuter.Services
 
             var validOutputArtifacts = await _storageService.VerifyObjectsExistAsync(workflowInstance.BucketId, artifactDict);
 
-            workflowInstance.Tasks?.ForEach(t =>
+            foreach (var artifact in artifactDict)
             {
-                if (t.TaskId == task.TaskId)
-                {
-                    t.OutputArtifacts = validOutputArtifacts;
-                }
-            });
+                _logger.LogArtifactPassing(new Artifact { Name = artifact.Key, Value = artifact.Value }, artifact.Value, "Post-Task Output Artifact", validOutputArtifacts.ContainsKey(artifact.Key));
+            }
+
+            var currentTask = workflowInstance.Tasks?.FirstOrDefault(t => t.TaskId == task.TaskId);
+
+            if (currentTask is not null)
+            {
+                currentTask.OutputArtifacts = validOutputArtifacts;
+            }
 
             if (validOutputArtifacts is not null && validOutputArtifacts.Any())
             {
