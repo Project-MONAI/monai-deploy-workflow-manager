019635fdea71f52e507ee772a10bb74c5ca3f3b3
Lillie Dae <lillie.dae@answerdigital.com>
2022-09-28 13:39:32 +0100

    code comment change
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

--- src/WorkflowManager/MonaiBackgroundService/Logging/Log.cs
+++ src/WorkflowManager/MonaiBackgroundService/Logging/Log.cs
@@ -18,10 +18,10 @@ namespace Monai.Deploy.WorkflowManager.MonaiBackgroundService.Logging
 {
     public static partial class Log
     {
-        [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Task {taskId} started at {startTime} and been running for {duration}, Timing out task. ExecutionId: {executionId}, CorrelationId: {correlationId}")]
+        [LoggerMessage(EventId = 1, Level = LogLevel.Warning, Message = "Task {taskId} started at {startTime} and been running for {duration}, Timing out task. ExecutionId: {executionId}, CorrelationId: {correlationId}")]
         public static partial void TimingOutTask(this ILogger logger, string taskId, string startTime, string duration, string executionId, string correlationId);
 
-        [LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "CancellationEvent triggered, Identity: {identity}, WorkflowInstanceId: {workflowInstanceId}")]
+        [LoggerMessage(EventId = 2, Level = LogLevel.Warning, Message = "CancellationEvent triggered, Identity: {identity}, WorkflowInstanceId: {workflowInstanceId}")]
         public static partial void TimingOutTaskCancellationEvent(this ILogger logger, string identity, string workflowInstanceId);
     }
 }
