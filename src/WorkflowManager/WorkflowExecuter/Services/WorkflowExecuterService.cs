eccbe2da4e8b641a3a9b360b58cb1108be4da082
RemakingEden <joss.sparkes@gmail.com>
2022-10-03 11:14:10 +0100

    Changes made with Lillie
    Signed-off-by: RemakingEden <joss.sparkes@gmail.com>

--- src/WorkflowManager/WorkflowExecuter/Services/WorkflowExecuterService.cs
+++ src/WorkflowManager/WorkflowExecuter/Services/WorkflowExecuterService.cs
@@ -704,6 +704,9 @@ namespace Monai.Deploy.WorkflowManager.WorkfowExecuter.Services
                 task.TimeoutMinutes = _defaultTaskTimeoutMinutes;
             }
 
+            var previousTask = workflowInstance.Tasks.First(task => task.TaskId == previousTaskId);
+            var previousTaskExecId = previousTask?.ExecutionId ?? executionId;
+
             return new TaskExecution()
             {
                 ExecutionId = executionId,
@@ -715,7 +718,7 @@ namespace Monai.Deploy.WorkflowManager.WorkfowExecuter.Services
                 Status = TaskExecutionStatus.Created,
                 Reason = FailureReason.None,
                 InputArtifacts = await _artifactMapper.ConvertArtifactVariablesToPath(task?.Artifacts?.Input ?? Array.Empty<Artifact>(), payloadId, workflowInstanceId, bucketName),
-                OutputDirectory = $"{payloadId}/workflows/{workflowInstanceId}/{executionId}",
+                OutputDirectory = $"{payloadId}/workflows/{workflowInstanceId}/{previousTaskExecId}",
                 ResultMetadata = { },
                 InputParameters = newInputParameters,
                 PreviousTaskId = previousTaskId ?? string.Empty,
