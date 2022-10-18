46feafd50071012378ff07f8472c9420bb8acb92
Sam Rooke <sam.rooke@answerdigital.com>
2022-10-18 14:55:22 +0100

    Check for required args in argo tasks
    Signed-off-by: Sam Rooke <sam.rooke@answerdigital.com>

--- tests/UnitTests/WorkflowManager.Tests/Validators/WorkflowValidatorTests.cs
+++ tests/UnitTests/WorkflowManager.Tests/Validators/WorkflowValidatorTests.cs
@@ -186,78 +186,84 @@ namespace Monai.Deploy.WorkflowManager.Test.Validators
             _workflowService.Setup(w => w.GetByNameAsync(It.IsAny<string>()))
                 .ReturnsAsync(new WorkflowRevision());
 
-            var workflowValidationResult = await this.WorkflowValidator.ValidateWorkflow(workflow);
+            for (var i = 0; i < 15; i++)
+            {
+                var workflowValidationResult = await this.WorkflowValidator.ValidateWorkflow(workflow);
 
-            Assert.True(workflowValidationResult.Errors.Count > 0);
+                Assert.True(workflowValidationResult.Errors.Count > 0);
 
-            Assert.Equal(16, workflowValidationResult.Errors.Count);
+                Assert.Equal(16, workflowValidationResult.Errors.Count);
 
-            var successPath = "rootTask => taskSucessdesc1 => taskSucessdesc2";
-            Assert.Contains(successPath, workflowValidationResult.SuccessfulPaths);
+                var successPath = "rootTask => taskSucessdesc1 => taskSucessdesc2";
+                Assert.Contains(successPath, workflowValidationResult.SuccessfulPaths);
 
-            var expectedConvergenceError = "Detected task convergence on path: rootTask => taskdesc1 => taskdesc2 => ∞";
-            Assert.Contains(expectedConvergenceError, workflowValidationResult.Errors);
+                var expectedConvergenceError = "Detected task convergence on path: rootTask => taskdesc1 => taskdesc2 => ∞";
+                Assert.Contains(expectedConvergenceError, workflowValidationResult.Errors);
 
-            var unreferencedTaskError = "Found Task(s) without any task destinations to it: taskdesc3,task_de.sc3?";
-            Assert.Contains(unreferencedTaskError, workflowValidationResult.Errors);
+                var unreferencedTaskError = "Found Task(s) without any task destinations to it: taskdesc3,task_de.sc3?";
+                Assert.Contains(unreferencedTaskError, workflowValidationResult.Errors);
 
-            var loopingTasksError = "Detected task convergence on path: rootTask => taskLoopdesc1 => taskLoopdesc2 => taskLoopdesc3 => taskLoopdesc4 => ∞";
-            Assert.Contains(loopingTasksError, workflowValidationResult.Errors);
+                var loopingTasksError = "Detected task convergence on path: rootTask => taskLoopdesc1 => taskLoopdesc2 => taskLoopdesc3 => taskLoopdesc4 => ∞";
+                Assert.Contains(loopingTasksError, workflowValidationResult.Errors);
 
-            var missingDestinationError = "Missing destination DoesNotExistDestination in task taskLoopdesc4";
-            Assert.Contains(missingDestinationError, workflowValidationResult.Errors);
+                var missingDestinationError = "Missing destination DoesNotExistDestination in task taskLoopdesc4";
+                Assert.Contains(missingDestinationError, workflowValidationResult.Errors);
 
-            var invalidTaskId = "TaskId: task_de.sc3? Contains Invalid Characters.";
-            Assert.Contains(invalidTaskId, workflowValidationResult.Errors);
+                var invalidTaskId = "TaskId: task_de.sc3? Contains Invalid Characters.";
+                Assert.Contains(invalidTaskId, workflowValidationResult.Errors);
 
-            var duplicateOutputArtifactName = "Task: \"rootTask\" has multiple output names with the same value.\n";
-            Assert.Contains(duplicateOutputArtifactName, workflowValidationResult.Errors);
+                var duplicateOutputArtifactName = "Task: \"rootTask\" has multiple output names with the same value.\n";
+                Assert.Contains(duplicateOutputArtifactName, workflowValidationResult.Errors);
 
-            var duplicateWorkflowName = $"A Workflow with the name: {workflow.Name} already exists.";
-            Assert.Contains(duplicateWorkflowName, workflowValidationResult.Errors);
+                var duplicateWorkflowName = $"A Workflow with the name: {workflow.Name} already exists.";
+                Assert.Contains(duplicateWorkflowName, workflowValidationResult.Errors);
 
-            WorkflowValidator.Reset();
+                WorkflowValidator.Reset();
+            }
         }
 
         [Fact]
         public async void ValidateWorkflow_ValidatesEmptyWorkflow_ReturnsErrorsAndHasCorrectValidationResultsAsync()
         {
-            var workflow = new Workflow();
+            for (var i = 0; i < 15; i++)
+            {
+                var workflow = new Workflow();
 
-            _workflowService.Setup(w => w.GetByNameAsync(It.IsAny<string>()))
-                .ReturnsAsync(new WorkflowRevision());
+                _workflowService.Setup(w => w.GetByNameAsync(It.IsAny<string>()))
+                    .ReturnsAsync(new WorkflowRevision());
 
-            var results = await this.WorkflowValidator.ValidateWorkflow(workflow);
+                var results = await this.WorkflowValidator.ValidateWorkflow(workflow);
 
-            Assert.True(results.Errors.Count > 0);
+                Assert.True(results.Errors.Count > 0);
 
-            Assert.Equal(8, results.Errors.Count);
+                Assert.Equal(8, results.Errors.Count);
 
-            var error1 = "'' is not a valid Workflow Description (source: Unnamed workflow).";
-            Assert.Contains(error1, results.Errors);
+                var error1 = "'' is not a valid Workflow Description (source: Unnamed workflow).";
+                Assert.Contains(error1, results.Errors);
 
-            var error2 = "'informaticsGateway' cannot be null (source: Unnamed workflow).";
-            Assert.Contains(error2, results.Errors);
+                var error2 = "'informaticsGateway' cannot be null (source: Unnamed workflow).";
+                Assert.Contains(error2, results.Errors);
 
-            var error3 = "'' is not a valid AE Title (source: informaticsGateway).";
-            Assert.Contains(error3, results.Errors);
+                var error3 = "'' is not a valid AE Title (source: informaticsGateway).";
+                Assert.Contains(error3, results.Errors);
 
-            var error4 = "'' is not a valid Informatics Gateway - exportDestinations (source: informaticsGateway).";
-            Assert.Contains(error4, results.Errors);
+                var error4 = "'' is not a valid Informatics Gateway - exportDestinations (source: informaticsGateway).";
+                Assert.Contains(error4, results.Errors);
 
-            var error5 = "Missing Workflow Name.";
-            Assert.Contains(error5, results.Errors);
+                var error5 = "Missing Workflow Name.";
+                Assert.Contains(error5, results.Errors);
 
-            var error6 = "Missing Workflow Version.";
-            Assert.Contains(error6, results.Errors);
+                var error6 = "Missing Workflow Version.";
+                Assert.Contains(error6, results.Errors);
 
-            var error7 = "Missing Workflow Tasks.";
-            Assert.Contains(error7, results.Errors);
+                var error7 = "Missing Workflow Tasks.";
+                Assert.Contains(error7, results.Errors);
 
-            var error8 = "A Workflow with the name:  already exists.";
-            Assert.Contains(error8, results.Errors);
+                var error8 = "A Workflow with the name:  already exists.";
+                Assert.Contains(error8, results.Errors);
 
-            WorkflowValidator.Reset();
+                WorkflowValidator.Reset();
+            }
         }
 
         [Fact]
@@ -356,9 +362,14 @@ namespace Monai.Deploy.WorkflowManager.Test.Validators
             _workflowService.Setup(w => w.GetByNameAsync(It.IsAny<string>()))
                 .ReturnsAsync(null, TimeSpan.FromSeconds(.1));
 
-            var results = await this.WorkflowValidator.ValidateWorkflow(workflow);
+            for (var i = 0; i < 15; i++)
+            {
+                var results = await this.WorkflowValidator.ValidateWorkflow(workflow);
 
-            Assert.True(results.Errors.Count == 0);
+                Assert.True(results.Errors.Count == 0);
+
+                WorkflowValidator.Reset();
+            }
         }
     }
 }
