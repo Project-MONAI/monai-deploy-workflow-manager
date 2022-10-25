33bfb38aa90278909c8b09ad1745b34d046569fc
Lillie Dae <61380713+lillie-dae@users.noreply.github.com>
2022-10-25 15:33:05 +0100

    fix issue validating workflows (#536)
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>
    
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

--- src/Shared/Shared/ValidationConstants.cs
+++ src/Shared/Shared/ValidationConstants.cs
@@ -16,30 +16,49 @@
 
 namespace Monai.Deploy.WorkflowManager.Shared
 {
-    public class ValidationConstants
+    public static class ValidationConstants
     {
         /// <summary>
         /// Key for the workflow name.
         /// </summary>
-        public const string ArgoWorkflowName = "workflow_name";
+        public static readonly string WorkflowName = "workflow_name";
 
         /// <summary>
         /// Key for the reviewed task id.
         /// </summary>
-        public const string ArgoReviewedTaskId = "reviewed_task_id";
+        public static readonly string ReviewedTaskId = "reviewed_task_id";
 
         /// <summary>
         /// Key for the queue name to send the clinical review message.
         /// </summary>
-        public const string ArgoQueueName = "queue_name";
+        public static readonly string QueueName = "queue_name";
 
         /// <summary>
-        /// Required arguments to run the Argo workflow.
+        /// Required arguments to run the clinical review task workflow args.
         /// </summary>
-        public static readonly IReadOnlyList<string> ArgoRequiredParameters = new List<string> {
-            ArgoQueueName,
-            ArgoWorkflowName,
-            ArgoReviewedTaskId
+        public static readonly IReadOnlyList<string> ClinicalReviewRequiredParameters = new List<string> {
+            QueueName,
+            WorkflowName,
+            ReviewedTaskId
         };
+
+        /// <summary>
+        /// Key for the endpoint where the Argo server is running.
+        /// </summary>
+        public static readonly string BaseUrl = "server_url";
+
+        /// <summary>
+        /// Key for the name of the main 'WorkflowTemplate' stored on the targeted Argo server.
+        /// </summary>
+        public static readonly string WorkflowTemplateName = "workflow_template_name";
+
+        /// <summary>
+        /// Required arguments to run the Argo task args.
+        /// </summary>
+        public static readonly IReadOnlyList<string> ArgoRequiredParameters =
+            new List<string> {
+                BaseUrl,
+                WorkflowTemplateName
+            };
     }
 }
