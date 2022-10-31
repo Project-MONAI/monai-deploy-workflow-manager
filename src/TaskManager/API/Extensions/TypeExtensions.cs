e5d1849d9ec3174c2f8253ede028bd576a42724a
RemakingEden <joss.sparkes@gmail.com>
2022-10-31 10:33:59 +0000

    Added missing files
    Signed-off-by: RemakingEden <joss.sparkes@gmail.com>

--- src/TaskManager/API/Extensions/TypeExtensions.cs
+++ src/TaskManager/API/Extensions/TypeExtensions.cs
@@ -1,6 +1,5 @@
 /*
  * Copyright 2022 MONAI Consortium
- * Copyright 2019-2020 NVIDIA Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
